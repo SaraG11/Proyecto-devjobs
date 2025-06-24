fa540685034820a0936f6f90788c9d36ffc07924
SG-WORK <gpsara1114@gmail.com>
2025-06-24 17:53:17 -0600

    Se puede postular a una vacante correctamente

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -14,6 +14,7 @@ class VacanteController extends Controller
      */
     public function index()
     {
+        $this->authorize('viewAny', Vacante::class);
         return view('vacantes.index');
     }
 
@@ -22,6 +23,7 @@ class VacanteController extends Controller
      */
     public function create()
     {
+        $this->authorize('create', Vacante::class);
         return view('vacantes.create');
     }
 
@@ -36,9 +38,11 @@ class VacanteController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Vacante $vacante)
     {
-        //
+        return view('vacantes.show', [
+            'vacante' => $vacante
+        ]);
     }
 
     /**
