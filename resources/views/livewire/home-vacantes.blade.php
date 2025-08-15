ef94d690bc3e52c0d07cb0ebeed94a10ec33cffd
SG-WORK <gpsara1114@gmail.com>
2025-08-14 18:39:10 -0600

    Finalizacion de proyecto devjobs, el sistema es funcional

--- resources/views/livewire/home-vacantes.blade.php
+++ resources/views/livewire/home-vacantes.blade.php
@@ -1,13 +1,16 @@
 <div>
+    <livewire:filtrar-vacantes />
     <div class="py-12">
         <div class="max-w-7xl mx-auto">
-            <h3 class="font-extrabold text-4xl text-gray-700 mb-12">Vacantes Disponibles</h3>
+            <h3 class="font-extrabold text-4xl text-gray-700 mb-12 px-2">Vacantes Disponibles</h3>
             <div class="bg-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
                 @forelse ($vacantes as $vacante)
                     <div class="md:flex md:justify-between md:items-center py-5">
                         <div class="md:flex-1">
                             <a class="text-2xl font-bold text-gray-600" href="{{route('vacantes.show', $vacante->id )}}">{{$vacante->titulo}}</a>
                             <p class="text-base text-gray-600 mb-2">{{ $vacante->empresa }}</p>
+                            <p class="text-xs font-bold text-gray-600 mb-2">{{ $vacante->categoria->categoria }}</p>
+                            <p class="text-base text-gray-600 mb-2">{{ $vacante->salario->salario }}</p>
                             <p class="font-bold text-xs text-gray-600">Último día para postularse:
                                 <span class="font-normal">{{ $vacante->ultimo_dia->format('d/m/Y')}}</span>
                             </p>
